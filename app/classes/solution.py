5c3f094fab6b4dfd821ff68ef4527bdaf961d70b
AdamZlocki <zlocki@student.agh.edu.pl>
2024-03-23 15:49:02 +0100

    Changed function calc_solution_time into a method

--- app/classes/solution.py
+++ app/classes/solution.py
@@ -1,6 +1,7 @@
 from typing import Dict, List
 
 from app.classes.vehicle import Vehicle
+# from app.classes.graph import GraphMatrix
 
 
 class Solution:
@@ -8,11 +9,13 @@ class Solution:
     do danego rozwiązania, LT to life time - charakterystyczne dla algorytmu pszczelego i niektórych innych więc nie
     wyrzucam na razie"""
 
-    def __init__(self, routes: Dict[Vehicle, List[int]], time: (float, int) = 0.0, LT: int = 0):
+    def __init__(self, routes: Dict[Vehicle, List[int]], times: Dict[Vehicle, (float, int)], time: (float, int) = 0.0,
+                 LT: int = 0):
         self.routes = routes  # zakładając że mamy więcej pojazdów niż 1 - kluczami w słowniku są id pojazdów,
         # a wartościami listy obsłużonych wierzchołków/pokonanych krawędzi
         self.time = time
         self.LT = LT
+        self.times = times
 
     def __eq__(self, other):
         if self.time == other.time:
@@ -31,3 +34,7 @@ class Solution:
             return True
         else:
             return False
+
+    def calc_solution_time(self):
+        """wybiera czas rowiązania na podstawie czasów każdego z pojazdów"""
+        self.time = max(self.times.values())
