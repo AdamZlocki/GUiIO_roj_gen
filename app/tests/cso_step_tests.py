61ddc0e045d82d6b2371d54469ce22d40d752018
Liero <dawidmazi@student.agh.edu.pl>
2024-03-23 14:24:02 +0100

    CSO step and distance function fix.

--- app/tests/cso_step_tests.py
+++ app/tests/cso_step_tests.py
@@ -1,4 +1,5 @@
 from app.classes.vehicle import Vehicle
+from app.classes.solution import Solution
 from app.utils.cso_functions import cso_step
 from app.utils.cso_functions import cso_get_step_distance
 
@@ -19,24 +20,23 @@ if __name__ == '__main__':
     # sol1 = {vehicles[0]: [1, 4], vehicles[1]: [5, 3, 2]}
     # sol2 = {vehicles[0]: [5, 1, 2], vehicles[1]: [4, 3]}
 
-    sol1 = {vehicles[0]: [2, 5], vehicles[1]: [9, 3, 8, 7], vehicles[2]: [4, 1, 6]}
-    sol2 = {vehicles[0]: [9, 8, 5, 1, 2], vehicles[1]: [4, 3], vehicles[2]: [7, 6]}
+    sol1 = Solution({vehicles[0]: [2, 5], vehicles[1]: [9, 3, 8, 7], vehicles[2]: [4, 1, 6]})
+    sol2 = Solution({vehicles[0]: [9, 8, 5, 1, 2], vehicles[1]: [4, 3], vehicles[2]: [7, 6]})
 
     print(f"Dystans w krokach = {cso_get_step_distance(sol1, sol2, vehicles)}")
 
     print("\nRozwiązanie do którego dążymy")
-    for route in sol2.values():
+    for route in sol2.routes.values():
         print(route)
 
     print("\nRozwiązanie które przemieszczamy")
-    for route in sol1.values():
+    for route in sol1.routes.values():
         print(route)
 
     no_steps = 0
     while sol1 != sol2:
-        cso_step(sol1, sol2, vehicles) # Wykonanie kroku
+        sol1 = cso_step(sol1, sol2, vehicles) # Wykonanie kroku
         no_steps += 1
         print(f"\nKrok {no_steps}")
-        for route in sol1.values():
+        for route in sol1.routes.values():
             print(route)
-
--- app/utils/cso_functions.py
+++ app/utils/cso_functions.py
@@ -1,7 +1,9 @@
-
-from app.classes.vehicle import Vehicle
 from typing import Dict, List
+from app.classes.solution import Solution
+from app.classes.vehicle import Vehicle
+from copy import deepcopy
 import random
+
 
 def dispersal(probability, actual_cockroach: Dict[Vehicle, List[int]], vehicle_order: List[Vehicle]):
     """Wywołanie rozproszenia z zadanym prawdopodobieństwem - Z każdego samochodu bierzemy losowe miasto i zamieniamy je z innym losowym samochodem"""
@@ -38,19 +40,13 @@ def dispersal(probability, actual_cockroach: Dict[Vehicle, List[int]], vehicle_o
         return actual_cockroach
     else:
         return actual_cockroach
-      
-from typing import Dict, List
-from app.classes.solution import Solution
-from app.classes.vehicle import Vehicle
-
-from copy import deepcopy
 
 
 class IncorrectSolutionException(Exception):
     pass
 
 
-def is_solution_acceptable(solution: Dict[Vehicle, List[int]]) -> bool:
+def are_routes_acceptable(solution: Dict[Vehicle, List[int]]) -> bool:
     """Zwraca True jeżeli wierzchołki się nie powtarzają i ich nie brakuje (oraz nie ma zera)"""
     vertex_list = []
     for route in solution.values():
@@ -64,31 +60,38 @@ def is_solution_acceptable(solution: Dict[Vehicle, List[int]]) -> bool:
     return True
 
 
-def is_solution_pair_acceptable(solution1: Dict[Vehicle, List[int]], solution2: Dict[Vehicle, List[int]]):
+def is_routes_pair_acceptable(solution1: Dict[Vehicle, List[int]], solution2: Dict[Vehicle, List[int]]):
     """Zwraca True jeżeli oba rozwiązania są poprawne, oraz zawierają taką samą liczbę wierzchołków"""
-    if not is_solution_acceptable(solution1) or not is_solution_acceptable(solution2):
+    if not are_routes_acceptable(solution1) or not are_routes_acceptable(solution2):
         return False
     if len([ver for route in solution1.values() for ver in route]) != len([ver for route in solution2.values() for ver in route]):
         return False
     return True
 
 
-def cso_step(cockroach_to_move: Dict[Vehicle, List[int]], destination: Dict[Vehicle, List[int]], vehicle_order: List[Vehicle]):
-    """Modyfikuje pierwszego karalucha (cockroach_to_move) robiąc krok w stronę drugiego (destination)"""
-    if not is_solution_pair_acceptable(cockroach_to_move, destination):
+def cso_step(cockroach_to_move: Solution, destination: Solution, vehicle_order: List[Vehicle]) -> Solution:
+    """Zwraca rozwiązanie powstałe w wyniku kroku pierwszego rozwiązania (cockroach_to_move) w stronę drugiego (destination)"""
+    cockroach_to_move_routes_copy = deepcopy(cockroach_to_move.routes)
+    _cso_route_step(cockroach_to_move_routes_copy, destination.routes, vehicle_order)
+    return Solution(cockroach_to_move_routes_copy)
+
+
+def _cso_route_step(cockroach_to_move_routes: Dict[Vehicle, List[int]], destination_routes: Dict[Vehicle, List[int]], vehicle_order: List[Vehicle]):
+    """Modyfikuje pierwszego karalucha (cockroach_to_move - ścieżki) robiąc krok w stronę drugiego (destination)"""
+    if not is_routes_pair_acceptable(cockroach_to_move_routes, destination_routes):
         raise IncorrectSolutionException
 
     for vehicle in vehicle_order:
-        ctm_vehicle, dest_vehicle = cockroach_to_move[vehicle], destination[vehicle]
+        ctm_vehicle, dest_vehicle = cockroach_to_move_routes[vehicle], destination_routes[vehicle]
         for i, dest_vertex in enumerate(dest_vehicle):
             if i > len(ctm_vehicle) - 1:
                 ctm_vehicle.append(dest_vertex)
-                _cso_step_remove_duplicate(cockroach_to_move, i, vehicle, dest_vertex, vehicle_order)
+                _cso_step_remove_duplicate(cockroach_to_move_routes, i, vehicle, dest_vertex, vehicle_order)
                 return
             elif dest_vertex != ctm_vehicle[i]:
                 val_to_replace = ctm_vehicle[i]
                 ctm_vehicle[i] = dest_vertex
-                _cso_step_remove_duplicate(cockroach_to_move, i, vehicle, dest_vertex, vehicle_order, val_to_replace)
+                _cso_step_remove_duplicate(cockroach_to_move_routes, i, vehicle, dest_vertex, vehicle_order, val_to_replace)
                 return
 
     # print("Rozwiązania są identyczne - nie wykonano kroku")
@@ -107,19 +110,23 @@ def _cso_step_remove_duplicate(sol_routes: Dict[Vehicle, List[int]], keep_vertex
                 return
 
 
-def cso_get_step_distance(cockroach_to_move: Dict[Vehicle, List[int]], destination: Dict[Vehicle, List[int]], vehicle_order: List[Vehicle], max_distance=100) -> int:
+def cso_get_step_distance(cockroach_to_move: Solution, destination: Solution, vehicle_order: List[Vehicle], max_distance=100) -> int:
     """Zwraca dystans w liczbie kroków między dwoma karaluchami.
     Kolejność działania ma znaczenie - dystans jest liczony z perspektywy pierwszego karalucha (pierwszego argumentu funkcji).
     Dystans może być inny jeżeli zamienimy karaluchy miejscami"""
-    if not is_solution_pair_acceptable(cockroach_to_move, destination):
+
+    cockroach_to_move_routes = cockroach_to_move.routes
+    destination_routes = destination.routes
+
+    if not is_routes_pair_acceptable(cockroach_to_move_routes, destination_routes):
         raise IncorrectSolutionException
 
-    cockroach_to_move_copy = deepcopy(cockroach_to_move)
+    cockroach_to_move_routes_copy = deepcopy(cockroach_to_move_routes)
     no_steps = 0
-    while cockroach_to_move_copy != destination:
+    while cockroach_to_move_routes_copy != destination_routes:
         if no_steps > max_distance:
             print("Przekroczono maksymalną liczbę kroków")
             return -1
-        cso_step(cockroach_to_move_copy, destination, vehicle_order)
+        _cso_route_step(cockroach_to_move_routes_copy, destination_routes, vehicle_order)
         no_steps += 1
     return no_steps
