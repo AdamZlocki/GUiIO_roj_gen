ca268f63e00163386be6761823042ac6ce38223e
Jakub Szczypek <szczypek@student.agh.edu.pl>
2024-03-24 11:18:34 +0100

    dispersal - return copy instead of changing the actual cockroach

--- app/utils/cso_functions.py
+++ app/utils/cso_functions.py
@@ -7,12 +7,12 @@ import random
 
 def dispersal(probability, actual_cockroach: Dict[Vehicle, List[int]], vehicle_order: List[Vehicle]):
     """Wywołanie rozproszenia z zadanym prawdopodobieństwem - Z każdego samochodu bierzemy losowe miasto i zamieniamy je z innym losowym samochodem"""
+    actual_cockroach_copy = deepcopy(actual_cockroach)
     generated_number = random.uniform(0, 0.99)
     if generated_number < probability:
         moved_elem = set()
-        # lst_of_vehicle_which_get_new_elem = set()
         for vehicle in vehicle_order:
-            cockroach_data = actual_cockroach[vehicle]
+            cockroach_data = actual_cockroach_copy[vehicle]
 
             random_index = random.randint(0, len(cockroach_data) - 1)
             random_element = cockroach_data[random_index]
@@ -25,21 +25,16 @@ def dispersal(probability, actual_cockroach: Dict[Vehicle, List[int]], vehicle_o
             moved_elem.add(random_element)
 
             other_vehicle = random.choice([v for v in vehicle_order if v != vehicle])
-            # Do wyjebania raczej
-            # Sprawdzanie czy wylosowany samochód dostał już jakiś element - Zabezpieczenie przed zmienieniem długości rozwiązań dla każdego samochodu
-            # while other_vehicle in lst_of_vehicle_which_get_new_elem:
-            #     other_vehicle = random.choice([v for v in vehicle_order if v != vehicle])
-            #
-            # lst_of_vehicle_which_get_new_elem.add(other_vehicle)
 
-            other_index = random.randint(0, len(actual_cockroach[other_vehicle]) - 1)
+            other_index = random.randint(0, len(actual_cockroach_copy[other_vehicle]) - 1)
 
-            actual_cockroach[vehicle].remove(random_element)
-            actual_cockroach[other_vehicle].insert(other_index, random_element)
+            actual_cockroach_copy[vehicle].remove(random_element)
+            actual_cockroach_copy[other_vehicle].insert(other_index, random_element)
 
-        return actual_cockroach
+        if are_routes_acceptable(actual_cockroach_copy):
+            return actual_cockroach_copy
     else:
-        return actual_cockroach
+        return actual_cockroach_copy
 
 
 class IncorrectSolutionException(Exception):
